94da601af113aaade2f295013135ec6f4dcfca1c
RsMan <William2000>
2020-11-11 15:29:21 +0100

    added models

---
+++ src/app/shared/models/todo/todo.ts
@@ -0,0 +1,12 @@
+// tslint:disable:variable-name
+import {Group} from '../group/group';
+import {Category} from '../category/category';
+
+export class Todo {
+  id: number;
+  label: string;
+  desc: string;
+  percent_done: number;
+  group: Group;
+  categories: Category[];
+}
