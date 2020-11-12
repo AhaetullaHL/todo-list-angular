0a4b0070c0cbe6def449e6d45593a63c0a3278b8
AhaetullaHL <hermann.ledent@gmail.com>
2020-11-12 15:11:23 +0100

    Update category model and service

--- src/app/shared/models/category/category.ts
+++ src/app/shared/models/category/category.ts
@@ -2,4 +2,14 @@ export class Category {
   id: number;
   label: string;
   color: string;
+
+  /**
+   * @param category: object
+   */
+  constructor(category?) {
+    category = category || {};
+    this.id = category.id || null;
+    this.label = category.label || '';
+    this.color = category.color || '';
+  }
 }
--- src/app/shared/services/category.service.ts
+++ src/app/shared/services/category.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import {Category} from "../models/category/category";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
+  categories: Category[] = [
+    new Category({
+      id: 1,
+      label: 'Dev',
+      color: 'red',
+    }),
+    new Category({
+      id: 2,
+      label: 'Prod',
+      color: 'blue',
+    }),
+    new Category({
+      id: 3,
+      label: 'Form',
+      color: 'green',
+    }),
+  ];
+
   constructor() { }
+
+  // get categories
+  getCategories() {
+    return this.categories;
+  }
+
+  //get catogory by index
+  getCategory(i){
+    return this.categories[i];
+  }
+
+  //add category
+  addCategory(category){
+    this.categories.push(category);
+  }
+
+  //delete category
+  deleteCategory(i){
+    this.categories.splice(i, 1);
+  }
+
+  //edit category
+  editCategory(value, i){
+    this.categories[i] = value;
+  }
 }
