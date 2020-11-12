0cc16f3c152613457734f2b65d1c064374d6d14f
AhaetullaHL <hermann.ledent@gmail.com>
2020-11-12 17:09:43 +0100

    Update table model and service

--- src/app/shared/models/table/table.ts
+++ src/app/shared/models/table/table.ts
@@ -4,4 +4,15 @@ export class Table {
   id: number;
   label: string;
   groups: Group[];
+
+  /**
+   *
+   * @param table: object
+   */
+  constructor(table?) {
+    table = table || {};
+    this.id = table.id || null;
+    this.label = table.label || '';
+    this.groups = table.groups || [];
+  }
 }
--- src/app/shared/services/table.service.ts
+++ src/app/shared/services/table.service.ts
@@ -1,9 +1,56 @@
 import { Injectable } from '@angular/core';
+import { Table } from "../models/table/table";
+import { GroupService } from "./group.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TableService {
 
-  constructor() { }
+  tables: Table[] = [
+    new Table({
+      id: 1,
+      label: 'table 1',
+      groups: [
+        this.groupService.getGroup(0),
+        this.groupService.getGroup(1),
+        this.groupService.getGroup(2),
+      ],
+    }),
+    new Table({
+      id: 2,
+      label: 'table 2',
+      groups: [
+        this.groupService.getGroup(1),
+      ],
+    }),
+
+  ];
+
+  constructor(private groupService: GroupService) { }
+
+  // get tables
+  getTables(){
+    return this.tables;
+  }
+
+  //get table
+  getTable(i){
+    return this.tables[i];
+  }
+
+  //add table
+  addTable(table){
+    this.tables.push(table);
+  }
+
+  //delete table
+  deleteTable(i){
+    this.tables.splice(i,1);
+  }
+
+  //edit table
+  editTable(value, i){
+    this.tables[i] = value;
+  }
 }
