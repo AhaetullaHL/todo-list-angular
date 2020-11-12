d3331e6f6e7c7e78f7f3a7810aab89471a0628ce
AhaetullaHL <hermann.ledent@gmail.com>
2020-11-12 16:36:26 +0100

    Update group model and service

--- src/app/shared/models/group/group.ts
+++ src/app/shared/models/group/group.ts
@@ -4,4 +4,15 @@ export class Group {
   id: number;
   label: string;
   todos: Todo[];
+
+  /**
+   *
+   * @param group: object
+   */
+  constructor(group?) {
+    group = group || {};
+    this.id = group.id || null;
+    this.label = group.label || '';
+    this.todos = group.todos || [];
+  }
 }
--- src/app/shared/services/group.service.ts
+++ src/app/shared/services/group.service.ts
@@ -1,9 +1,62 @@
 import { Injectable } from '@angular/core';
+import { TodoService } from "./todo.service";
+import { Group } from "../models/group/group";
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupService {
 
-  constructor() { }
+  groups: Group[] = [
+    new Group({
+      id: 1,
+      label: 'ToDo',
+      todos: [
+        this.todoService.getTodo(0),
+        this.todoService.getTodo(1),
+        this.todoService.getTodo(2),
+      ],
+    }),
+    new Group({
+      id: 2,
+      label: 'Current',
+      todos: [
+        this.todoService.getTodo(3),
+        this.todoService.getTodo(4),
+      ],
+    }),
+    new Group({
+      id: 3,
+      label: 'Finish',
+      todos: [],
+    }),
+
+  ];
+
+  constructor(private todoService: TodoService) { }
+
+  // get groups
+  getGroups(){
+    return this.groups
+  }
+
+  //get group
+  getGroup(i){
+    return this.groups[i];
+  }
+
+  //add group
+  addGroup(group){
+    this.groups.push(group);
+  }
+
+  //delete group
+  deleteGroup(i){
+    this.groups.splice(i, 1);
+  }
+
+  //edit group
+  editGroup(value, i){
+    this.groups[i] = value;
+  }
 }
