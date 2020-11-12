133d52ac0a46f5cc86e7db9deddf6c53ef3ed2e6
AhaetullaHL <hermann.ledent@gmail.com>
2020-11-12 11:55:55 +0100

    Create templates components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import {LoginComponent} from "./components/auth/login/login.component";
-import {RegisterComponent} from "./components/auth/register/register.component";
-import {TodoListComponent} from "./components/todo-list/todo-list.component";
+import { LoginComponent } from "./components/auth/login/login.component";
+import { RegisterComponent } from "./components/auth/register/register.component";
+import { TodoListComponent } from "./components/todo-list/todo-list.component";
+import { NavComponent } from './components/templates/nav/nav.component';
+import { GroupComponent } from './components/templates/group/group.component';
+import { CardComponent } from './components/templates/card/card.component';
+import { FormComponent } from './components/templates/form/form.component';
 
 @NgModule({
   declarations: [
@@ -14,8 +18,11 @@ import {TodoListComponent} from "./components/todo-list/todo-list.component";
     HomeComponent,
     LoginComponent,
     RegisterComponent,
-    TodoListComponent
-
+    TodoListComponent,
+    NavComponent,
+    GroupComponent,
+    CardComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
