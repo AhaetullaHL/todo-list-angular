640bbb635831510b5ba554871bf3e277e2827c26
AhaetullaHL <hermann.ledent@gmail.com>
2020-11-14 16:45:22 +0100

    Add CRUD methods in group service

--- src/app/shared/services/group/group.service.ts
+++ src/app/shared/services/group/group.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
-import { TodoService } from "../todo/todo.service";
 import { Group } from "../../models/group/group";
-import { environment as env } from "../../../../environments/environment";
-import { Observable } from "rxjs";
 import { HttpClient } from "@angular/common/http";
+import {RequestService} from "../request/request.service";
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +9,35 @@ import { HttpClient } from "@angular/common/http";
 export class GroupService {
 
 
-  constructor(private todoService: TodoService, private http: HttpClient) { }
+  constructor(private http: HttpClient, private requestService: RequestService) { }
 
+  getAll(callback: (groups: Group[]) => void): void{
+    this.requestService.get<Group[]>('groups', {}, true, data => {
+      callback(data);
+    });
+  }
+
+  add(label: string, callback: (groups: Group[]) => void): void{
+    this.requestService.post<Group>('groups', {}, {label}, true, data => {
+      callback(data);
+    });
+  }
+
+  get(id: number, callback: (groups: Group) => void): void{
+    this.requestService.get<Group>(`groups/${id}`, {}, true, data => {
+      callback(data);
+    });
+  }
+
+  edit(id: number, label: string, callback: (groups: Group) => void): void{
+    this.requestService.put(`groups/${id}`, {}, {label}, true, data => {
+      callback(data);
+    });
+  }
+
+  delete(id: number, callback: (groups: Group) => void): void{
+    this.requestService.delete(`groups/${id}`, {}, true, data => {
+      data;
+    });
+  }
 }
